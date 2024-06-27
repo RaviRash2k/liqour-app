982848f93efcd5a12e338b42fd6242d74ea7ad91
RaviRash2k <ravindurashmithapg@gmail.com>
2024-06-27 17:47:08 +0530

    new files

--- app/src/main/java/com/example/testingmad/adminFragments/HomeFragment.java
+++ app/src/main/java/com/example/testingmad/adminFragments/HomeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -20,54 +21,16 @@ import com.example.testingmad.R;
 
 public class HomeFragment extends Fragment {
 
-    ListView listView;
-    String[] title = {"aaa", "bbb", "ccc", "ddd", "eee"};
-    String[] price = {"10", "20", "30", "40", "50"};
-    int[] img = {R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background};
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        listView = rootView.findViewById(R.id.lst);
-        Adapter adapter = new Adapter(getContext(), title, img, price);
-        listView.setAdapter(adapter);
+        recyclerView = rootView.findViewById(R.id.rview1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         return rootView;
-    }
-}
-
-class Adapter extends ArrayAdapter<String>{
-    Context context;
-    int[] images;
-    String[] titles;
-    String[] price;
-
-    Adapter(Context context, String[] title, int[] images, String[] price){
-
-        super(context, R.layout.single_item, R.id.forName, title);
-        this.context = context;
-        this.images = images;
-        titles = title;
-        this.price = price;
-    }
-
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View row = inflater.inflate(R.layout.single_item, parent, false);
-
-        ImageView imageView = row.findViewById(R.id.forimg);
-        TextView titleView = row.findViewById(R.id.forName);
-        TextView priceView = row.findViewById(R.id.forPrice);
-
-        imageView.setImageResource(images[position]);
-        titleView.setText(titles[position]);
-        priceView.setText(price[position]);
-
-        return row;
     }
 }
